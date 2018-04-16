2a6ba2d0b42c09b62a5bcd3191d4a82d3ed8f063
Justin Tooley <jstooley823@gmail.com>
2018-04-16 22:40:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ class Employer {
 
   deliveries(){
     let allDeliveries = this.employees().map(employee => {return employee.deliveries()});
-    let merged = [...[], ...allDeliveries};
+    let merged = [...[], ...allDeliveries];
     return merged;
   }
 
